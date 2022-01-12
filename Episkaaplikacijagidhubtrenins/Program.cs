5e3c8f7ce344c696481bf4112355a92ba1ea7657
dimuslv <95738452+dimuslv@users.noreply.github.com>
2022-01-12 08:39:45 +0200

    Add project files.

---
+++ Episkaaplikacijagidhubtrenins/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Episkaaplikacijagidhubtrenins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cau no Mragususa\n");
+        }
+    }
+}
